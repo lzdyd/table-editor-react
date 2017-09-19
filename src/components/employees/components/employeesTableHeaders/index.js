df19be29c5c105c31550cc83d5ab30090cc0e33d
Artur Chernysh <Artur@achernysh.tsc.ts>
2017-09-19 18:06:14 +0300

    '.'

--- src/components/employees/components/employeesTableHeaders/index.js
+++ src/components/employees/components/employeesTableHeaders/index.js
@@ -5,12 +5,12 @@ import './style.scss';
 export default class EmployeesTableHeaders extends Component {
   render() {
     const tableHeaders = this.props.tableHeader.map((item, i) => {
-      return <div className="table-cell table-cell-headers" key={i} data-field={item}>{item}</div>;
+      return <div className="table-cell table-cell-headers" key={ i } data-field={ item }>{ item }</div>;
     });
 
     return (
       <div className="table-row table-row-headers">
-        {tableHeaders}
+        { tableHeaders }
       </div>
     );
   }
